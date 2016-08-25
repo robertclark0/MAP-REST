3be85201bc7596dad7d72c4f8767b216f8158967
Robert <robertclark0@gmail.com>
2016-08-25 09:15:07 -0500

    added routes to controller for report functions

--- Controllers/Report.Controller.cs
+++ Controllers/Report.Controller.cs
@@ -17,20 +17,60 @@ namespace MAP_REST.Controllers
         [HttpPost]
         public HttpResponseMessage Report([FromBody] dynamic postObject)
         {
+            var db = new ReportDataContext();
+            var result = new List<Models.Report>();
+            result = db.getReport(postObject.report.GUID);
 
-            switch ((string)postObject.type)
-            {
-                case "reportList":
-                    break;
-                case "getReport":
-                    break;
-                case "updateReport":
-                    break;
-                case "deleteReport":
-                    break;
-            }
+            Log.ServerLog(Log.GenerateServerSessionID(), "report", Log.SerializeObject(result), postObject);
+            return Request.CreateResponse(HttpStatusCode.OK, new { result });
+        }
 
-            return Request.CreateResponse(HttpStatusCode.OK, "reports");
+        [Route("report/update")]
+        [HttpPost]
+        public HttpResponseMessage Update([FromBody] dynamic postObject)
+        {
+            var db = new ReportDataContext();
+            db.updateReport(postObject.report.GUID, postObject.report.user, postObject.report.name, postObject.report.type, postObject.report.json);
+            var result = "updated";
+
+            Log.ServerLog(Log.GenerateServerSessionID(), "report/update", Log.SerializeObject(postObject.report.GUID), postObject);
+            return Request.CreateResponse(HttpStatusCode.OK, new { result });
+        }
+
+        [Route("report/create")]
+        [HttpPost]
+        public HttpResponseMessage Create([FromBody] dynamic postObject)
+        {
+            var db = new ReportDataContext();
+            db.createReport(postObject.entityCode, postObject.report.GUID, postObject.report.user, postObject.report.name, postObject.report.type, postObject.report.json);
+            var result = "created";
+
+            Log.ServerLog(Log.GenerateServerSessionID(), "report/create", Log.SerializeObject(postObject.report.GUID), postObject);
+            return Request.CreateResponse(HttpStatusCode.OK, new { result });
+        }
+
+        [Route("report/delete")]
+        [HttpPost]
+        public HttpResponseMessage Delete([FromBody] dynamic postObject)
+        {
+            var db = new ReportDataContext();
+            db.deleteReport(postObject.report.GUID);
+            var result = "deleted";
+
+            Log.ServerLog(Log.GenerateServerSessionID(), "report/delete", Log.SerializeObject(postObject.report.GUID), postObject);
+            return Request.CreateResponse(HttpStatusCode.OK, new { result });
+        }
+
+        [Route("report/list")]
+        [HttpPost]
+        public HttpResponseMessage List([FromBody] dynamic postObject)
+        {
+            var db = new ReportDataContext();
+            var result = new List<Models.ReportList>();
+            result = db.getReportList(postObject.entityCode);
+
+            Log.ServerLog(Log.GenerateServerSessionID(), "report/list", Log.SerializeObject(result), postObject);
+            return Request.CreateResponse(HttpStatusCode.OK, new { result });
         }
 
     }
