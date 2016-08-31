330e93502f8b4ec5e933f288b019fb6180c66322
Robert <robertclark0@gmail.com>
2016-08-31 14:44:47 -0500

    demo update

--- DEMO/CHUP.model.cs
+++ DEMO/CHUP.model.cs
@@ -7,10 +7,8 @@ namespace MAP_REST.DEMO
 {
     public class CHUP
     {
-        public string REPORT_DATE { get; set; }
-        public string REGION { get; set; }
-        public int CNT { get; set; }
-        public string DMIS_ID { get; set; }
+        //public string REPORT_DATE { get; set; }
+
         public string DMIS_NAME { get; set; }
         public string MED_HOME_MEPRS { get; set; }
         public string PCM_NAME { get; set; }
@@ -18,15 +16,18 @@ namespace MAP_REST.DEMO
         public string SPON_SSN { get; set; }
         public string SSN { get; set; }
         public string GENDER { get; set; }
-        public string PAY_PLAN { get; set; }
-        public string PAY_GRD { get; set; }
+        //public string PAY_PLAN { get; set; }
+        //public string PAY_GRD { get; set; }
         public string EDIPN { get; set; }
-        public string BEN_CAT { get; set; }
+        //public string BEN_CAT { get; set; }
         public int AGE { get; set; }
         public string LNAME { get; set; }
         public string FNAME { get; set; }
         public string NAME { get; set; }
         public string PROD_NAME { get; set; }
         public int OPIATE_CNT { get; set; }
+        public string REGION { get; set; }
+        public int CNT { get; set; }
+        public string DMIS_ID { get; set; }
     }
 }
