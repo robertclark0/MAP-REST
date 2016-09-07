12495a48b620c9a3ecd0a0f30881e1912c08c314
Robert <robertclark0@gmail.com>
2016-09-07 13:58:02 -0500

    Select string, halfway

--- _QueryBuilder/QueryBuilder.cs
+++ _QueryBuilder/QueryBuilder.cs
@@ -12,24 +12,77 @@ namespace MAP_REST.QueryBuilder
             return "";
         }
 
-        private string[] BuildSelections(dynamic queryOjbect)
+        private string[] BuildQuery(dynamic queryOjbect)
         {
-            var querySelections = new string[queryOjbect.selections.length];
+            var querySelections = new List<string>();
+            var queryGrouping = new List<string>();
+            var queryOrdering = new List<string>();
+
             bool aggregateFlag = queryOjbect.aggregation.enabled;
+            bool paginationFlag = queryOjbect.pagination.enabled;
 
             foreach (dynamic selection in queryOjbect.selections)
             {
                 if (aggregateFlag)
                 {
-
+                    if (selection.aggregate)
+                    {
+                        switch ((string)selection.aggregate.type)
+                        {
+                            case "count":
+                                querySelections.Add(SelectCount(selection));
+                                break;
+                            case "sum":
+                                querySelections.Add(SelectSum(selection));
+                                break;
+                            case "case-count":
+                                querySelections.Add(SelectCaseCount(selection));
+                                break;
+                            case "case-sum":
+                                querySelections.Add(SelectCaseSum(selection));
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        querySelections.Add(Select(selection));
+                        queryOrdering.Add(Order(selection));
+                    }
                 }
                 else
                 {
-                    
+                    querySelections.Add(Select(selection));
+                    queryOrdering.Add(Order(selection));
                 }
             }
 
-            return querySelections;
+            return querySelections.ToArray();
+        }
+
+        private string Select(dynamic selection)
+        {
+            return String.Format("[{0}]", selection.name);
+        }
+        private string SelectCount(dynamic selection)
+        {
+            return String.Format("COUNT([{0}]) AS [{0}]", selection.aggregate.allias);
+        }
+        private string SelectSum(dynamic selection)
+        {
+            return String.Format("SUM([{0}]) AS [{0}]", selection.aggregate.allias);
+        }
+        private string SelectCaseCount(dynamic selection)
+        {
+            return String.Format("SUM(CASE WHEN [{0}] THEN 1 ELSE 0 END) AS [{1}]", null, selection.aggregate.allias);
+        }
+        private string SelectCaseSum(dynamic selection)
+        {
+            return String.Format("ROUND(SUM(CASE WHEN [{0}] THEN [{1}] ELSE 0 END),{2}) AS [{3}]", null, selection.name, selection.aggregate.round, selection.aggregate.allias);
+        }
+
+        private string Order(dynamic selection)
+        {
+            return String.Format("{0} {1}", selection.name, selection.order);
         }
     }
 }
