5480bfffcfd6214414d19b5aea76b9bdb962aa6a
Robert <robertclark0@gmail.com>
2016-09-07 13:23:19 -0500

    started query builder

---
+++ _QueryBuilder/QueryBuilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MAP_REST.QueryBuilder
+{
+    public class QueryBuilder
+    {
+        public string BuildQueryString(dynamic queryOjbect)
+        {
+            return "";
+        }
+
+        private string[] BuildSelections(dynamic queryOjbect)
+        {
+            var querySelections = new string[queryOjbect.selections.length];
+            bool aggregateFlag = queryOjbect.aggregation.enabled;
+
+            foreach (dynamic selection in queryOjbect.selections)
+            {
+                if (aggregateFlag)
+                {
+
+                }
+                else
+                {
+                    
+                }
+            }
+
+            return querySelections;
+        }
+    }
+}
