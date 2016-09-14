fbace30cf545a6da6bfaa30a859db5f62d24313a
Robert <robertclark0@gmail.com>
2016-09-14 14:53:18 -0500

    added hangfire

---
+++ Startup.cs
@@ -0,0 +1,20 @@
+﻿using Hangfire;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MAP_REST.Startup))]
+
+namespace MAP_REST
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+		    GlobalConfiguration.Configuration
+                .UseSqlServerStorage("Hangfire");
+
+			app.UseHangfireDashboard();
+			app.UseHangfireServer();
+        }
+    }
+}
