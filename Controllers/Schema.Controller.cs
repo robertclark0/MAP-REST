c15e9bccb0e2c2386eec23d846a06167be3f490a
Robert <robertclark0@gmail.com>
2016-09-22 15:07:21 -0500

    added schema data for columns

--- Controllers/Schema.Controller.cs
+++ Controllers/Schema.Controller.cs
@@ -14,7 +14,7 @@ namespace MAP_REST.Controllers
     {
         [Route("schema/table")]
         [HttpPost]
-        public HttpResponseMessage Query([FromBody] dynamic postObject)
+        public HttpResponseMessage tableSchema([FromBody] dynamic postObject)
         {
             string entityCode = postObject.post.entityCode;
             string tableName = postObject.post.tableName;
@@ -24,6 +24,23 @@ namespace MAP_REST.Controllers
             var connection = Credentials.getConnectionString("MASTER", "U");
             var db = new SchemaDataContext(connection.ConnectionString);
             result = db.getTableSchema(entityCode, tableName);
+
+
+            return Request.CreateResponse(HttpStatusCode.OK, new { result });
+        }
+
+        [Route("schema/column")]
+        [HttpPost]
+        public HttpResponseMessage columnDistinct([FromBody] dynamic postObject)
+        {
+            string entityCode = postObject.post.entityCode;
+            string tableName = postObject.post.tableName;
+            string columnName = postObject.post.columnName;
+            
+            var connection = Credentials.getConnectionString("MASTER", "U");
+            var db = new SchemaDataContext(connection.ConnectionString);
+
+            var result = db.getColumnDistinct(entityCode, tableName, columnName);
 
 
             return Request.CreateResponse(HttpStatusCode.OK, new { result });
--- DataAccess/Schema.DataContext.cs
+++ DataAccess/Schema.DataContext.cs
@@ -11,7 +11,7 @@ namespace MAP_REST.DataAccess
 {
     public class SchemaDataContext : DbContext
     {
-        public SchemaDataContext(string nameOrConnectionString = "name=Master")
+        public SchemaDataContext(string nameOrConnectionString)
             : base(nameOrConnectionString)
         { }
 
@@ -25,6 +25,29 @@ namespace MAP_REST.DataAccess
             string SQL = "SELECT [COLUMN_NAME],[DATA_TYPE] FROM [MAP_{0}].INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{1}' ORDER BY 1";
 
             return this.Database.SqlQuery<Models.Schema>(String.Format(SQL, entityCode, tableName)).ToList();
+        }
+
+        public dynamic getColumnDistinct(string entityCode, string tableName, string columnName)
+        {
+            var items = new List<List<object>>();
+
+            this.Database.Connection.Open();
+            var cmd = this.Database.Connection.CreateCommand();
+            cmd.CommandText = String.Format("SELECT DISTINCT [{0}] FROM [MAP_{1}].dbo.[{2}]", columnName, entityCode, tableName);
+
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var item = new List<Object>();
+                items.Add(item);
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    item.Add(reader[i]);
+                }
+            }
+
+            return items;
         }
 
 
